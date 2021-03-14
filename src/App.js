c9bcddaf2b527421d844ea742b9eae5b74528d5a
Brandon Ha <hsy0602@outlook.com>
2021-03-14 17:44:01 +0900

    project start

--- src/App.js
+++ src/App.js
@@ -1,16 +1,5 @@
-import React from "react";
-import GlobalStyle from "./globalStyles";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Navbar } from "./components";
-
 function App() {
-  return (
-    <Router>
-      <GlobalStyle /> 
-      <Navbar />
-      <Switch></Switch>
-    </Router>
-  );
+  return <div className="App">oxinion</div>;
 }
 
 export default App;
