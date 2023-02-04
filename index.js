92a786fdcb2d8acb4bda04b63c37b9280840df1f
Brandon Ha <idevbrandon@gmail.com>
2023-02-04 15:33:38 +0900

    build basic functions

---
+++ index.js
@@ -0,0 +1,12 @@
+const noble = require("noble");
+
+const oxinion = {
+  startScan: function() {
+    console.log("Scanning started.");
+  },
+  stopScan: function() {
+    console.log("Scanning stopped.");
+  }
+};
+
+export default oxinion;
