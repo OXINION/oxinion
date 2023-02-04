aa351218aa61771a519575f72b8bf4a19275d2c3
Brandon Ha <idevbrandon@gmail.com>
2023-02-04 17:37:25 +0900

    megaphone & pay

--- test/script.js
+++ test/script.js
@@ -4,3 +4,19 @@ oxinion.startScan();
 oxinion.onDiscover((address) => {
   console.log(`Discovered device with address: ${address}`);
 });
+
+oxinion.megaphoneForCoupon("Save 20% on all items!");
+
+const paymentData = {
+  uuid: "your_payment_service_uuid",
+  data: "your_payment_data",
+};
+
+oxinion
+  .scanAndPay(paymentData)
+  .then((message) => {
+    console.log(message);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
