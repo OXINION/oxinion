a989dc469abc7556f17e663f0a5539d03384a004
Brandon Ha <idevbrandon@gmail.com>
2023-02-04 17:37:58 +0900

    create new features

--- index.js
+++ index.js
@@ -12,6 +12,40 @@ const oxinion = {
       callback(peripheral.address);
     });
   },
+  megaphoneForCoupon: function (couponMessage) {
+    console.log(`Starting to broadcast coupon message: ${couponMessage}`);
+
+    noble.startAdvertising(couponMessage, () => {
+      console.log(
+        `Coupon message "${couponMessage}" is now being broadcasted.`
+      );
+    });
+  },
+
+  scanAndPay(paymentData) {
+    return new Promise((resolve, reject) => {
+      noble.startScanning();
+      noble.on("discover", (peripheral) => {
+        // Check if the peripheral has the desired payment service
+        if (peripheral.uuid === paymentData.uuid) {
+          // Connect to the peripheral and send the payment information
+          peripheral.connect((error) => {
+            if (error) {
+              reject(error);
+            } else {
+              peripheral.writeData(paymentData.data, (error) => {
+                if (error) {
+                  reject(error);
+                } else {
+                  resolve("Payment Successful!");
+                }
+              });
+            }
+          });
+        }
+      });
+    });
+  },
 };
 
 export default oxinion;
