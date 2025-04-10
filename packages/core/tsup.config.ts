3c430dbca4cbf10e7840be485cb47d032df0993c
Brandon Ha <idevbrandon@gmail.com>
2025-04-10 15:23:22 +0900

    update

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -1,9 +1,20 @@
 // packages/core/tsup.config.ts
-import { defineConfig } from 'tsup';
+import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ['src/index.ts'],
-  format: ['esm', 'cjs'],
+  entry: ["src/index.ts"],
+  format: ["esm", "cjs"],
   dts: true,
   clean: true,
+  splitting: false,
+  sourcemap: true,
+  minify: true,
+  treeshake: true,
+  platform: "neutral",
+  target: "es2017",
+  outExtension({ format }) {
+    return {
+      js: `.${format === "esm" ? "mjs" : "js"}`,
+    };
+  },
 });
